aa82a7bb32013d988c3d7da59c8c43ca43350d69
veexy <bondarila27@gmail.com>
2023-11-15 16:25:40 +0300

    feat: add array with paragraphs for pay widget

--- src/components/shared/consts.tsx
+++ src/components/shared/consts.tsx
@@ -4,3 +4,11 @@ export const headerLinks: LinkType[] = [
   { name: "Вебинары", link: "/webinars" },
   { name: "Войти", link: "/login" },
 ];
+export const borderDesign = "border border-white rounded-xl ";
+export const frontSidePayWidgetParagraph = [
+  "Гарантируем трудоустройство — или вернём деньги",
+  "Студенты могут вернуть до 13% стоимости курса, оформив налоговый вычет",
+  "Бессрочный доступ ко всему контенту курса",
+  "15+ проектов в портфолио",
+  "Интерактивный тренажёр в Figma",
+];
