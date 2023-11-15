d4a2a706078f1a6d87bcb821fb135ee49fa3564b
veexy <bondarila27@gmail.com>
2023-11-15 16:26:37 +0300

    feat: add dynamic title

--- src/widgets/paywidget/components/Title.tsx
+++ src/widgets/paywidget/components/Title.tsx
@@ -1,4 +1,8 @@
-import { PropsWithChildren } from "react";
-export function Title({ children }: PropsWithChildren) {
-  return <h1>{children}</h1>;
+import { Dispatch } from "../../../components/shared/types";
+export function Title({ open }: Dispatch) {
+  return (
+    <h1 className="font-montserrat font-bold text-3xl text-center p-[50px]">
+      {!open ? "Стоимость курса" : "Оплата"}
+    </h1>
+  );
 }
