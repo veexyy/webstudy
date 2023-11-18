8948495751874e6dd4e952eb9c3b772314cdc737
veexy <bondarila27@gmail.com>
2023-11-18 15:44:10 +0300

    fix: fix padding

--- src/widgets/paywidget/components/Title.tsx
+++ src/widgets/paywidget/components/Title.tsx
@@ -1,7 +1,7 @@
 import { Dispatch } from "../../../components/shared/types";
 export function Title({ open }: Dispatch) {
   return (
-    <h1 className="font-montserrat font-bold text-3xl text-center p-[50px]">
+    <h1 className="font-montserrat font-bold sm:text-3xl text-2xl text-center sm:p-[50px] p-[30px]">
       {!open ? "Стоимость курса" : "Оплата"}
     </h1>
   );
