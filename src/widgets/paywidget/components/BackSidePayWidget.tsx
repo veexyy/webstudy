e1493a428b4cf5860ee9490844bffdf137cd81a4
veexy <bondarila27@gmail.com>
2023-11-18 15:40:23 +0300

    feat: complete backside widget

--- src/widgets/paywidget/components/BackSidePayWidget.tsx
+++ src/widgets/paywidget/components/BackSidePayWidget.tsx
@@ -1,8 +1,19 @@
-import { Title } from "./Title";
-export function BackSidePayWidget() {
+import { PropsWithChildren } from "react";
+import BackSidePayWidgetInfo from "./BackSidePayWidgetInfo";
+import PayForm from "./PayForm";
+import LoginThroughVK from "./LoginThroughVK";
+
+export function BackSidePayWidget({ children }: PropsWithChildren) {
   return (
-    <div className="text-white">
-      <Title>Оплата</Title>
+    <div className="text-white relative">
+      <div className="flex sm:gap-10 items-center flex-col sm:flex-row">
+        <BackSidePayWidgetInfo>{children}</BackSidePayWidgetInfo>
+      </div>
+      <div className="font-montserrat font-bold my-2 sm:my-6 text-center">
+        Заполните контактные данные
+      </div>
+      <LoginThroughVK />
+      <PayForm />
     </div>
   );
 }
