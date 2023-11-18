efeee9da767f2bffb83165a8510fb8e924c0d223
veexy <bondarila27@gmail.com>
2023-11-18 15:43:50 +0300

    fix: add greeting message

--- src/widgets/paywidget/components/PayButton.tsx
+++ src/widgets/paywidget/components/PayButton.tsx
@@ -1,21 +1,35 @@
 import { borderDesign } from "../../../components/shared/consts";
-import { Dispatch } from "../../../components/shared/types";
-
-export default function PayButton({ open, setOpen }: Dispatch) {
+import { useState } from "react";
+type PayButtonType = {
+  isValid: boolean;
+};
+export default function PayButton({ isValid }: PayButtonType) {
+  const [showMessage, setShowMessage] = useState(false);
+  const handleClick = () => {
+    setShowMessage(true);
+  };
   return (
     <>
+      {showMessage && (
+        <p className="absolute text-blue-600 font-montserrat text-xs -top-3 sm:top-0">
+          Мы отправили вам заявку!
+        </p>
+      )}
       <button
+        disabled={!isValid}
+        type="submit"
+        value="Оплатить"
+        onClick={handleClick}
         className={
-          borderDesign +
-          "px-16 py-2 font-montserrat font-bold mt-20 mb-16 w-fit "
+          isValid
+            ? borderDesign +
+              "px-16 py-2 font-montserrat font-bold mb-8 mt-3 sm:my-8 sm:w-fit hover:border-orange-500 duration-500 text-xs sm:text-base"
+            : borderDesign +
+              "px-16 py-2 font-montserrat font-bold mb-8 mt-3 sm:my-8 sm:w-fit duration-500 text-xs sm:text-base " +
+              "border border-gray-400 text-gray-600  duration-500"
         }
-        onClick={() => setOpen(!open)}
       >
-        {!open ? (
-          "Перейти к оплате"
-        ) : (
-          <a href="https://web.telegram.org/k/#@fuidhf">Оплатить</a>
-        )}
+        Оплатить
       </button>
     </>
   );
