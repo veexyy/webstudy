465370b44cb0364574356f4ea426df430b5553ed
veexy <bondarila27@gmail.com>
2023-11-20 14:45:51 +0300

    feat: add array for widget with achievements

--- src/components/shared/consts.tsx
+++ src/components/shared/consts.tsx
@@ -4,15 +4,16 @@ export const headerLinks: LinkType[] = [
   { name: "Вебинары", link: "/webinars" },
   { name: "Войти", link: "/login" },
 ];
-export const borderDesign = "border border-white rounded-xl ";
-export const frontSidePayWidgetParagraph = [
+export const borderDesign: string = "border border-white rounded-xl ";
+export const frontSidePayWidgetParagraph: string[] = [
   "Гарантируем трудоустройство — или вернём деньги",
   "Студенты могут вернуть до 13% стоимости курса, оформив налоговый вычет",
   "Бессрочный доступ ко всему контенту курса",
   "15+ проектов в портфолио",
   "Интерактивный тренажёр в Figma",
 ];
-export const choosePaymentType = [
+export const choosePaymentType: string[] = [
   "Взять в рассрочку",
   "Оплатить сумму целиком",
 ];
+export const ourAchievments: string[] = ["356  курсов", "190 000+ студентов"];
