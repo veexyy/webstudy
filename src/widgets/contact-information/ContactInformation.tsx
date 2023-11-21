81f6a6eedcedad1cd2336b73e383825568fbcae1
veexy <bondarila27@gmail.com>
2023-11-21 15:14:56 +0300

    fix: add responsive design

--- src/widgets/contact-information/ContactInformation.tsx
+++ src/widgets/contact-information/ContactInformation.tsx
@@ -1,13 +1,15 @@
 import WidgetTitle from "../../components/shared/widgettitle";
 import { useForm } from "react-hook-form";
 import { FormValues } from "../paywidget/components/PayForm";
+import { useState } from "react";
 export function ContactInformation() {
+  const [showMessage, setShowMessage] = useState(false);
   const {
     register,
     handleSubmit,
     trigger,
     reset,
-    formState: { errors },
+    formState: { errors, isValid },
   } = useForm<FormValues>();
   const onSubmit = (data: FormValues) => {
     console.log(data);
@@ -16,24 +18,24 @@ export function ContactInformation() {
   const inputStyle =
     "border border-white bg-transparent rounded-xl p-3 ring-2 ring-blue-600 focus:outline-none w-full";
   return (
-    <div className="flex flex-col border-white border rounded-md bg-transparent mx-auto max-w-[900px] pb-10 pl-10 pr-10">
+    <div className="flex flex-col border-white border rounded-md bg-transparent mx-auto max-w-[1100px] pb-10 pl-10 pr-10">
       <WidgetTitle>Контактная информация</WidgetTitle>
-      <div className=" font-montserrat text-white border border-white rounded-md p-5 mt-10">
-        <div className="flex justify-between items-center my-10">
-          <div className="font-bold text-2xl">
+      <div className="font-montserrat text-white border border-white rounded-md sm:p-5 pb-5 mt-10">
+        <div className="flex flex-col sm:flex-row justify-between items-center sm:my-10 my-5 gap-5 sm:gap-12">
+          <div className="font-bold text-xl sm:text-2xl ml-5 md:ml-10">
             Поможем в выборе!
-            <div className="text-xl">Даже если очень сложно</div>
+            <div className="sm:text-xl text-sm">Даже если очень сложно</div>
           </div>
           <form
             onSubmit={handleSubmit(onSubmit)}
-            className="grid grid-flow-row-dense grid-cols-2 gap-7"
+            className="flex flex-col sm:grid grid-flow-row-dense grid-cols-2 sm:gap-7 gap-5"
           >
             <div className="relative col-span-2">
               <input
                 {...register("name", {
                   pattern: {
                     value:
-                      /^[a-zA-Zа-яА-ЯёЁ]+(([' -][a-zA-Zа-яА-ЯёЁ ])?[a-zA-Zа-яА-ЯёЁ]*)*$/,
+                      /^[a-zA-Zа-яА-ЯёЁ]+(([' -][a-zA-Zа-яА-ЯёЁ ])?[a-zA-Zа-яА-ЯёЁ]*) *$/,
                     message: "Неправильное имя",
                   },
                   required: "Укажи имя",
@@ -87,14 +89,25 @@ export function ContactInformation() {
                 {errors.email?.message}
               </p>
             </div>
+            <div className=" relative col-span-2">
+              <input
+                className={
+                  isValid
+                    ? "border border-white px-12 py-2 rounded-md font-bold cursor-pointer mt-5 w-full"
+                    : "border border-gray-400 px-12 py-2 rounded-md font-bold text-gray-400 mt-5 w-full"
+                }
+                type="submit"
+                value="Отправить"
+                disabled={!isValid}
+                onClick={() => setShowMessage(true)}
+              />
+              {showMessage && (
+                <p className="absolute text-blue-600 font-montserrat text-xs -top-2">
+                  Мы отправили вам заявку!
+                </p>
+              )}
+            </div>
           </form>
-        </div>
-        <div className="flex justify-center">
-          <input
-            className="border border-white px-12 py-2 rounded-md font-bold cursor-pointer"
-            type="submit"
-            value="Отправить"
-          />
         </div>
       </div>
     </div>
