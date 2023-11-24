ff7901cc73a96fda9b0bdb8a96d81695eb83dfe8
veexy <107259651+veexyy@users.noreply.github.com>
2023-11-24 14:38:07 +0300

    feat: add section what you will can after course (#17)

---
+++ src/widgets/what-you-will-can/components/Title.tsx
@@ -0,0 +1,7 @@
+export default function Title() {
+  return (
+    <div className="text-white font-montserrat font-bold text-2xl md:text-3xl mt-2 md:ml-5">
+      Что ты будешь уметь:
+    </div>
+  );
+}
