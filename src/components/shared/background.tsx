731fc6fc74d2b667a97f39946d6e448a5e4a74f9
veexy <bondarila27@gmail.com>
2023-12-04 16:01:55 +0300

    fix: add more numbers

--- src/components/shared/background.tsx
+++ src/components/shared/background.tsx
@@ -1,7 +1,7 @@
 export default function Background() {
   const arr: number[] = [
     0, 1, 10, 100, 11, 111, 1000, 1001, 1111, 11111, 10000, 10001, 11111,
-    111111, 100001,
+    111111, 100001, 1000001, 1111111, 11111111, 10000001, 100000001,
   ];
   function shuffleArray(arr: number[]) {
     for (let i = 0; i < 5000; i++) {
@@ -10,7 +10,7 @@ export default function Background() {
   }
   shuffleArray(arr);
   return (
-    <div className="font-akony font-bold text-white select-none h-full w-[110%] fixed bg-black top-0 opacity-5 break-words">
+    <div className="font-akony font-bold text-white select-none h-full w-[110%] fixed top-0 opacity-5 break-words">
       {arr.map((item, i) => (
         <span key={i}>{item}</span>
       ))}
