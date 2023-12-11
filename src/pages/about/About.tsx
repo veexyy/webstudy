433d315b5070f094f14ef9f9b2de96bcf770d402
veexy <bondarila27@gmail.com>
2023-12-11 17:51:58 +0300

    feat: add some pages

---
+++ src/pages/about/About.tsx
@@ -0,0 +1,3 @@
+export default function About() {
+  return <div className="text-white relative z-[1] max-w-[1650px]">About</div>;
+}
---
+++ src/pages/login/login-page.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from "../../widgets/login/LoginForm";
+
+export default function LoginPage() {
+  return (
+    <>
+      <LoginForm />
+    </>
+  );
+}
