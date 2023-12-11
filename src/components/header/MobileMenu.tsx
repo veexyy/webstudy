4ab44d3f387cb253be9bfc489994d613d3d64284
veexy <bondarila27@gmail.com>
2023-12-11 17:38:13 +0300

    fix: bug fixes

--- src/components/header/MobileMenu.tsx
+++ src/components/header/MobileMenu.tsx
@@ -4,19 +4,32 @@ import Cross from "./Cross";
 import { headerLinks } from "../shared/consts";
 
 export function MobileMenu({ open, setOpen }: Dispatch) {
+  const token = localStorage.getItem("tokens");
+  {
+    token ? headerLinks.splice(2, 1) : null;
+  }
+
   return (
     <>
       <Cross open={open} setOpen={setOpen} />
-      <div className="lg:invisible visible duration-1000 absolute bg-black h-full w-full top-0 left-0 text-white text-center flex justify-center font-bold items-center font-montserrat gap-[10px]">
+      <div className="lg:invisible visible duration-1000 absolute bg-black min-h-[100vh] w-full top-0 left-0 text-white text-center flex justify-center font-bold items-center font-montserrat gap-[10px] z-50">
         <ul className="flex flex-col gap-[10px]">
           {headerLinks.map(({ link, name }, i) => (
             <li
+              onClick={() => setOpen(!open)}
               key={i}
               className="hover:underline underline-offset-4 duration-500"
             >
               <Link to={link}>{name}</Link>
             </li>
           ))}
+          {token ? (
+            <li onClick={() => localStorage.removeItem("tokens")}>Выйти</li>
+          ) : (
+            <Link to={"/login"} onClick={() => setOpen(!open)}>
+              <li>Войти</li>
+            </Link>
+          )}
         </ul>
       </div>
     </>
