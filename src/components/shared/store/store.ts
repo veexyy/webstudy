eae129d2ca089562a53feca99d6d1888f366bcc7
veexy <bondarila27@gmail.com>
2023-12-21 18:27:37 +0300

    feat: add slice for filters

--- src/components/shared/store/store.ts
+++ src/components/shared/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
+import filterReducer from "./filterSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    courseFilters: filterReducer,
   },
 });
 
