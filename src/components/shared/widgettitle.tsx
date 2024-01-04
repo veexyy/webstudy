f1c5c1918a2df63b7f3e78b24d835f1d34036d4e
veexy <bondarila27@gmail.com>
2024-01-04 20:28:01 +0300

    fix: style fix

--- src/components/shared/widgettitle.tsx
+++ src/components/shared/widgettitle.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 export default function WidgetTitle({ children }: PropsWithChildren) {
   return (
-    <h1 className="text-white text-center font-montserrat text-2xl font-bold my-4 md:my-[40px]">
+    <h1 className="text-white text-center font-montserrat text-2xl font-bold my-4 lg:my-[40px]">
       {children}
     </h1>
   );
