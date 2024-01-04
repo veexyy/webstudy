e283d87109de59ca690af1dadfba10846c327471
veexy <107259651+veexyy@users.noreply.github.com>
2024-01-04 15:43:18 +0300

    Feat/about us (#31)
    * fix: fix header width
    
    * feat: add about us page
    
    * fix: footer gap fix
    
    * fix: styles fix
    
    * fix: styles fix
    
    * fix: styles fix

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,7 +5,7 @@ import styles from "./styles.module.css";
 export function Footer() {
   return (
     <footer>
-      <div className="flex flex-col lg:flex-row bg-transparent justify-between px-3 items-center lg:px-6 desktop:px-[50px] h-fit max-w-[1548px] mx-auto relative z-[1]">
+      <div className="flex flex-col lg:flex-row bg-transparent justify-between px-3 items-center lg:px-6 desktop:px-[50px] h-fit max-w-[1548px] mx-auto relative pb-10 z-[1]">
         <div className="lg:w-1/5">
           <Logo />
         </div>
