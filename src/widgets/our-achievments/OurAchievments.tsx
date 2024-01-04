3957600f2d5367a47511c320dbcdb903f1944003
veexy <bondarila27@gmail.com>
2024-01-04 15:41:56 +0300

    fix: styles fix

--- src/widgets/our-achievments/OurAchievments.tsx
+++ src/widgets/our-achievments/OurAchievments.tsx
@@ -4,9 +4,9 @@ import { ourAchievments } from "../../components/shared/consts";
 
 export function OurAchievments() {
   return (
-    <div className="border-white border rounded-md  bg-transparent max-w-[900px] mx-auto">
+    <div className="border-white border rounded-md  bg-transparent max-w-[1100px]  mx-auto">
       <WidgetTitle> Наши достижения </WidgetTitle>
-      <div className="flex sm:gap-10 gap-5 px-4 md:px-0 justify-center sm:my-[70px] my-7">
+      <div className="flex sm:gap-10 gap-5 px-4 md:px-0 justify-center sm:my-[100px] my-7">
         {ourAchievments.map((achievment, i) => (
           <OurAchievmentsBlock key={i}>{achievment}</OurAchievmentsBlock>
         ))}
--- src/widgets/our-achievments/components/achievment-block.tsx
+++ src/widgets/our-achievments/components/achievment-block.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 export default function AchievmentBlock({ children }: PropsWithChildren) {
   return (
-    <div className="bg-black border border-white text-white w-[300px]  rounded-md text-center font-montserrat mobile:py-6 py-3 mobile:text-2xl text-xl">
+    <div className="bg-black border border-white text-white w-[300px] flex justify-center items-center rounded-md text-center font-montserrat mobile:py-6 py-3 mobile:text-2xl text-xl">
       {children}
     </div>
   );
