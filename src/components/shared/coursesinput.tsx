2af082f9d1e04ecf0cca1156d7a4e5e524400ac5
veexy <bondarila27@gmail.com>
2024-01-05 16:49:35 +0300

    fix: input fix

--- src/components/shared/coursesinput.tsx
+++ src/components/shared/coursesinput.tsx
@@ -1,27 +1,18 @@
 import { SlMagnifier } from "react-icons/sl";
-import { useState, useEffect } from "react";
-import { useLocation } from "react-router";
-export default function Coursesinput() {
-  const [placeholder, setPlaceholder] = useState("");
-  const location = useLocation();
-  useEffect(() => {
-    if (location.pathname === "/courses") {
-      setPlaceholder("Поиск по курсам");
-    } else {
-      setPlaceholder("Поиск по вебинарам");
-    }
-  });
 
+export default function Coursesinput({ search, setSearch }: any) {
   return (
     <>
       <div className="relative">
         <>
           <SlMagnifier className="absolute fill-white top-3 left-2 scale-[1, -1]" />
           <input
+            value={search}
             type="text"
-            name="search"
+            name="courses-search"
             className="bg-transparent border border-white rounded-xl font-montserrat py-2 pl-8 pr-32 text-white focus:outline-none"
-            placeholder={placeholder}
+            placeholder="Поиск по курсам"
+            onChange={(e) => setSearch(e.target.value)}
           />
         </>
       </div>
