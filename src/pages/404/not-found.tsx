4aceb7ed6bfb5a66ac1f83e6281766ed459ab8de
veexy <bondarila27@gmail.com>
2024-01-11 19:59:34 +0300

    feat: stylize the not found page

--- src/pages/404/not-found.tsx
+++ src/pages/404/not-found.tsx
@@ -1,3 +1,15 @@
+import { Link } from "react-router-dom";
+
 export default function NotFound() {
-  return <div className="text-white">404</div>;
+  return (
+    <div className="flex flex-col justify-center items-center gap-16 text-white relative z-[1] h-screen">
+      <h1 className="font-akony text-7xl lg:text-8xl">404</h1>
+      <h2 className="text-2xl font-montserrat font-bold">
+        Страница не найдена
+      </h2>
+      <h3 className="text-xl font-montserrat font-bold">
+        <Link to="/">Вернуться на главную</Link>
+      </h3>
+    </div>
+  );
 }
