853c7adb9a7c14bb104dd73fe2590bb81b12795d
veexy <bondarila27@gmail.com>
2024-01-11 19:57:10 +0300

    fix: some fixes

--- src/components/shared/coursesinput.tsx
+++ src/components/shared/coursesinput.tsx
@@ -3,14 +3,14 @@ import { SlMagnifier } from "react-icons/sl";
 export default function Coursesinput({ search, setSearch }: any) {
   return (
     <>
-      <div className="relative">
+      <div className="relative w-full">
         <>
           <SlMagnifier className="absolute fill-white top-3 left-2 scale-[1, -1]" />
           <input
             value={search}
             type="text"
             name="courses-search"
-            className="bg-transparent border border-white rounded-xl font-montserrat py-2 pl-8 pr-32 text-white focus:outline-none"
+            className="bg-transparent border w-full lg:w-auto border-white rounded-xl font-montserrat py-2 pl-8 pr-16 lg:pr-[90px] text-white focus:outline-none"
             placeholder="Поиск по курсам"
             onChange={(e) => setSearch(e.target.value)}
           />
