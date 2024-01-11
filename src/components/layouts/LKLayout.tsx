6399b9c79915e6c790cc27c94966ed73fcd0d2bd
veexy <bondarila27@gmail.com>
2024-01-11 19:52:50 +0300

    feat: add lazy loading with spinners

--- src/components/layouts/LKLayout.tsx
+++ src/components/layouts/LKLayout.tsx
@@ -1,12 +1,23 @@
+import { Suspense } from "react";
 import { LKHeader } from "../lkheader/LKHeader";
 import { Outlet } from "react-router-dom";
-
-export function LKLayout() {
+import { TailSpin } from "react-loader-spinner";
+export default function LKLayout() {
   return (
     <>
-      <LKHeader />
-      <div className="relative z-[1] max-w-[1536px] px-3 lg:px-6 desktop:px-[30px] mx-auto">
-        <Outlet />
+      <div className="max-w-[1548px] mx-auto">
+        <LKHeader />
+      </div>
+      <div className="relative max-w-[1536px] px-3 lg:px-6 desktop:px-[30px] h-full mx-auto">
+        <Suspense
+          fallback={
+            <div className="flex flex-col justify-center items-center min-h-screen">
+              <TailSpin height={100} width={300} color="white" />
+            </div>
+          }
+        >
+          <Outlet />
+        </Suspense>
       </div>
     </>
   );
--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,15 +1,25 @@
 import { Header } from "../header/Header";
 import { Footer } from "../footer/Footer";
 import { Outlet } from "react-router-dom";
+import { Suspense } from "react";
+import { TailSpin } from "react-loader-spinner";
 
-export const Layout = () => {
+export default function Layout() {
   return (
     <>
       <Header />
       <div className="relative z-[1] max-w-[1536px] px-3 lg:px-6 desktop:px-[30px] mx-auto">
-        <Outlet />
+        <Suspense
+          fallback={
+            <div className="flex flex-col justify-center items-center min-h-screen">
+              <TailSpin height={100} width={300} color="white" />
+            </div>
+          }
+        >
+          <Outlet />
+        </Suspense>
       </div>
       <Footer />
     </>
   );
-};
+}
